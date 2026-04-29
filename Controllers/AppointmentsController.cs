6bb456e12052b1fc338d9fc93c9dd7ca57d0e3d5
nt-tracy <s33507@pjwstk.edu.pl>
2026-04-29 17:16:27 +0200

    Added first endpoint - GET /api/appointments

---
+++ Controllers/AppointmentsController.cs
@@ -0,0 +1,66 @@
+﻿using C06_APBD.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using C06_APBD.DTOs;
+
+namespace C06_APBD.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AppointmentsController :ControllerBase
+{
+    private readonly string _connectionString ="Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ClinicAdoNet;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll([FromQuery] string? status, [FromQuery] string? patientLastName)
+    {
+        var appointments = new List<AppointmentListDto>();
+
+        await using var connection = new SqlConnection(_connectionString);
+        await using var command = new SqlCommand("""
+            SELECT 
+                a.IdAppointment, 
+                a.AppointmentDate, 
+                a.Status, 
+                a.Reason, 
+                p.FirstName + ' ' + p.LastName AS PatientFullName, 
+                p.Email AS PatientEmail
+            FROM dbo.Appointments a
+            JOIN dbo.Patients p ON p.IdPatient = a.IdPatient
+            WHERE (@Status IS NULL OR a.Status = @Status)
+              AND (@PatientLastName IS NULL OR p.LastName LIKE @PatientLastName + '%')
+            ORDER BY a.AppointmentDate;
+            """, connection);
+
+        command.Parameters.AddWithValue("@Status", (object?)status ?? DBNull.Value);
+        command.Parameters.AddWithValue("@PatientLastName", (object?)patientLastName ?? DBNull.Value);
+
+        await connection.OpenAsync();
+        await using var reader = await command.ExecuteReaderAsync();
+
+        while (await reader.ReadAsync())
+        {
+            appointments.Add(new AppointmentListDto
+            {
+                IdAppointment = (int)reader["IdAppointment"],
+                AppointmentDate = (DateTime)reader["AppointmentDate"],
+                Status = (string)reader["Status"],
+                Reason = (string)reader["Reason"],
+                PatientFullName = (string)reader["PatientFullName"],
+                PatientEmail = (string)reader["PatientEmail"]
+            });
+        }
+
+        return Ok(appointments);
+    } 
+    
+    
+    
+    
+    
+    
+}
+
+    
+    
